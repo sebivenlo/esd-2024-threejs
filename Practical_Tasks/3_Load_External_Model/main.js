af1a062ce519804fcb64a3c337d6b6181d0c4d8b
MonaLewin <93583774+MonaLewin@users.noreply.github.com>
2024-10-09 15:37:40 +0200

    ref: move solution to solutions and add todos to task 3

--- Practical_Tasks/3_Load_External_Model/main.js
+++ Practical_Tasks/3_Load_External_Model/main.js
@@ -3,40 +3,10 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
-const scene = new THREE.Scene()
-scene.background = new THREE.Color("#ffffff")
-
-const light = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add( light );
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
-camera.position.set(1.5, 0.75, 2)
-
-const renderer = new THREE.WebGLRenderer({ antialias: true })
-
-renderer.shadowMap.enabled = true
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
+//TODOS:
+//1. Setup scene, light, camera, renderer and controls
 
 
-new GLTFLoader().load('Model/shiba/scene.gltf', (gltf) => {
-  console.log(gltf)
+//2. Load provided GLTF file from model folder and add it to the scene (you can also use your own file)
 
-  scene.add(gltf.scene)
-
-})
-
-
-function animate() {
-  requestAnimationFrame(animate)
-
-  controls.update()
-
-  renderer.render(scene, camera)
-
-}
-
-animate()
+//3. Animate the model
---
+++ Practical_Tasks/Solutions/3_Load_External_Model.js
@@ -0,0 +1,42 @@
+
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+
+const scene = new THREE.Scene()
+scene.background = new THREE.Color("#ffffff")
+
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
+camera.position.set(1.5, 0.75, 2)
+
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+
+renderer.shadowMap.enabled = true
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+
+
+new GLTFLoader().load('Model/shiba/scene.gltf', (gltf) => {
+  console.log(gltf)
+
+  scene.add(gltf.scene)
+
+})
+
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  controls.update()
+
+  renderer.render(scene, camera)
+
+}
+
+animate()
