f064967ec742df98a1ec072fd0368d929c49b0b9
MeganSpielberg <481584@student.fontys.nl>
2024-10-23 17:16:58 +0200

    Update Task 4 solution

--- Practical_Tasks/Solutions/4_Loading_Screen.js
+++ Practical_Tasks/Solutions/4_Loading_Screen.js
@@ -1,67 +1,66 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 // An object to hold all the things needed for our loading screen
 var loadingScreen = {
-  scene: new THREE.Scene(),
-  camera: new THREE.PerspectiveCamera(90, 1280 / 720, 0.1, 100),
-  box: new THREE.Mesh(
-    new THREE.BoxGeometry(0.5, 0.5, 0.5),
-    new THREE.MeshBasicMaterial({ color: 0x4444ff })
-  )
+	scene: new THREE.Scene(),
+	camera: new THREE.PerspectiveCamera(90, 1280/720, 0.1, 100),
+	box: new THREE.Mesh(
+		new THREE.BoxGeometry(0.5,0.5,0.5),
+		new THREE.MeshBasicMaterial({ color:0x4444ff })
+	)
 };
 var loadingManager = new THREE.LoadingManager();
 var RESOURCES_LOADED = false;
 
 // Set up the loading screen's scene.
-// It can be treated just like our main scene.
-loadingScreen.box.position.set(0, 0, 5);
-loadingScreen.camera.lookAt(loadingScreen.box.position);
-loadingScreen.scene.add(loadingScreen.box);
+	// It can be treated just like our main scene.
+	loadingScreen.box.position.set(0,0,5);
+	loadingScreen.camera.lookAt(loadingScreen.box.position);
+	loadingScreen.scene.add(loadingScreen.box);
+	
+	// Create a loading manager to set RESOURCES_LOADED when appropriate.
+	loadingManager.onLoad = function(){
+		RESOURCES_LOADED = true;
+	};
 
-// Create a loading manager to set RESOURCES_LOADED when appropriate.
-loadingManager.onLoad = function () {
-  RESOURCES_LOADED = true;
-};
 
-//Scene, camera and renderer are mandatory to render the scene with a camera
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 scene.background = new THREE.Color("#ffffff")
 
-const light = new THREE.AmbientLight(0x404040); // soft white light
-scene.add(light);
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
 camera.position.set(1.5, 0.75, 2)
-camera.lookAt(0, 0, 0)
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
+const renderer = new THREE.WebGLRenderer({ antialias: true })
 
-// This line inserts the Three.JS object (Canvas) into the web pages HTML Body. renderer.domElement creates a <Canvas> 
-document.body.appendChild(renderer.domElement);
+renderer.shadowMap.enabled = true
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-new GLTFLoader().load('Model/shiba/scene.gltf', (gltf) => {
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+
+var loader = new GLTFLoader(loadingManager)
+loader.load('Model/shiba/scene.gltf', (gltf) => {
   console.log(gltf)
 
   scene.add(gltf.scene)
 
 })
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.autoRotate = true;
-controls.autoRotateSpeed = 1;
-controls.update();
-
 function animate() {
   // This block runs while resources are loading.
-  if (RESOURCES_LOADED == false) {
-    requestAnimationFrame(animate);
-    renderer.render(loadingScreen.scene, loadingScreen.camera);
-    return; // Stop the function here.
-  }
-
+	if( RESOURCES_LOADED == false ){
+		requestAnimationFrame(animate);
+		renderer.render(loadingScreen.scene, loadingScreen.camera);
+		return; // Stop the function here.
+	}
+  
   requestAnimationFrame(animate)
 
   controls.update()
